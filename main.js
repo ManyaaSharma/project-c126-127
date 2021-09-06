4e33b5a9b008d0b23bff6b1e6d1283152d2cb9aa
✩clvudii✩ <78642658+cluvdii@users.noreply.github.com>
2021-09-06 20:54:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreRightWrist=0;
+scoreLeftWrist=0;
 song="";
 score=0
 
@@ -30,14 +32,40 @@ function draw(){
     image(video, 0 , 0, 600, 500);
     fill("#FF0000");
     stroke("#FF0000");
+
+    if(scoreRightWrist>0.2){
+
+    circle(rightWristX,rightWristY,20);
+
+    if(rightWristY>0 &&rightWristY<=100){
+        document.getElementById("speed").innerHTML="speed=0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY>100 && rightWristY<=200){
+        document.getElementById("speed").innerHTML="speed=1x";
+        song.rate(1);
+    }
+    else if(rightWristY>200 && rightWristY<=300){
+        document.getElementById("speed").innerHTML="speed=1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY>300 && rightWristY<=400){
+        document.getElementById("speed").innerHTML="speed=2x";
+        song.rate(2);
+    }
+    else if(rightWristY>400 && rightWristY<=500){
+        document.getElementById("speed").innerHTML="speed=2.5x";
+        song.rate(2.5);
+    }
+}
     if(score>0.2){
         circle(leftWristX,leftWristY,20);
     inNumberLeftWristY=Number(leftWristY);
-    removeDecimals=Math.floor(inNumberLeftWristY);
+    removeDecimals=floor(inNumberLeftWristY);
     volume=removeDecimals/500;
     song.setVolume(volume);
-    }
     document.getElementById("volume").innerHTML="volume= "+volume;
+    }  
 }
 
 function modelLoaded(){
@@ -48,6 +76,7 @@ function gotPoses(results){
     if(results.length>0){
         console.log(results);
         scoreLeftWrist=results[0].pose.keypoints[9].score;
+        scoreRightWrist=results[0].pose.keypoints[10].score;
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX="+leftWristX+",leftWristY="+leftWristY);
